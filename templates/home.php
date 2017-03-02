e902322ca2c6f7336f47991a113bee4d55b3e094
MaggGomes <magg.slb@gmail.cm>
2017-03-02 10:34:23 +0000

    first version

---
+++ templates/home.php
@@ -0,0 +1,58 @@
+<section class = "central">
+    <div class="container">
+        <article class="listrestaurant box">
+            <h4>New Article</h4>
+            <h5>March 1</h5>
+            <br>
+            <img class="pressimg" src="../res/images/press.jpg">
+            <br>
+            <p>
+                Lorem ipsum dolor sit amet, at tation vocibus platonem nam, id cum eligendi deseruisse, mea clita vitae sanctus no. Legere debitis vel ne. Impetus vivendo voluptatibus vim ne. Audire tacimates sea id, labores volumus scribentur eos no. Ridens putent maluisset ei nam, has et purto recusabo, dicat labitur electram pro id. Offendit luptatum tincidunt ne mea, per no discere placerat nominati.
+
+                Pro no nonumy saperet. Volumus argumentum et vel, at nam possim habemus. Postea volumus concludaturque eam eu, cum labore iisque ex. Cu illud causae facilis mei, mea ei mucius timeam corpora. Has reque persequeris an, cum in tale choro urbanitas.
+
+                Te erant vulputate eam, debitis verterem qui no, sed denique torquatos ex. Vocent denique adipisci pri ei, et est appareat percipit mnesarchum. Pri quot theophrastus et. Cum eirmod partiendo consequuntur cu. Te atqui ullamcorper qui, nec atomorum ocurreret ex, eu utinam quidam eos.
+
+                Ne postulant efficiantur vel, everti regione sit cu, option albucius accusamus cum cu. Graeci contentiones has et, ut ius aliquip imperdiet consetetur, nullam ignota propriae in vel. Quo magna sententiae in, diam duis cu nec. Has no nostrud mnesarchum, ad aliquid dolorem nec.
+
+                At quod facer praesent mei. Id doctus molestie his, justo solet per id. Dicat volumus postulant ne his, soleat veritus referrentur te cum, mel no elitr tamquam numquam. Sint consectetuer vix at, cu vim nullam neglegentur. His ei quot errem laoreet, nam ei vidit dicunt. Ad repudiare dignissim mea, ei eum rebum rationibus adipiscing.
+            </p>
+        </article>
+        <article class="listrestaurant box">
+            <h4>New Article</h4>
+            <h5>March 1</h5>
+            <br>
+            <img class="pressimg" src="../res/images/press.jpg">
+            <br>
+            <p>
+                Lorem ipsum dolor sit amet, at tation vocibus platonem nam, id cum eligendi deseruisse, mea clita vitae sanctus no. Legere debitis vel ne. Impetus vivendo voluptatibus vim ne. Audire tacimates sea id, labores volumus scribentur eos no. Ridens putent maluisset ei nam, has et purto recusabo, dicat labitur electram pro id. Offendit luptatum tincidunt ne mea, per no discere placerat nominati.
+
+                Pro no nonumy saperet. Volumus argumentum et vel, at nam possim habemus. Postea volumus concludaturque eam eu, cum labore iisque ex. Cu illud causae facilis mei, mea ei mucius timeam corpora. Has reque persequeris an, cum in tale choro urbanitas.
+
+                Te erant vulputate eam, debitis verterem qui no, sed denique torquatos ex. Vocent denique adipisci pri ei, et est appareat percipit mnesarchum. Pri quot theophrastus et. Cum eirmod partiendo consequuntur cu. Te atqui ullamcorper qui, nec atomorum ocurreret ex, eu utinam quidam eos.
+
+                Ne postulant efficiantur vel, everti regione sit cu, option albucius accusamus cum cu. Graeci contentiones has et, ut ius aliquip imperdiet consetetur, nullam ignota propriae in vel. Quo magna sententiae in, diam duis cu nec. Has no nostrud mnesarchum, ad aliquid dolorem nec.
+
+                At quod facer praesent mei. Id doctus molestie his, justo solet per id. Dicat volumus postulant ne his, soleat veritus referrentur te cum, mel no elitr tamquam numquam. Sint consectetuer vix at, cu vim nullam neglegentur. His ei quot errem laoreet, nam ei vidit dicunt. Ad repudiare dignissim mea, ei eum rebum rationibus adipiscing.
+            </p>
+        </article>
+        <article class="listrestaurant box">
+            <h4>New Article</h4>
+            <h5>March 1</h5>
+            <br>
+            <img class="pressimg" src="../res/images/press.jpg">
+            <br>
+            <p>
+                Lorem ipsum dolor sit amet, at tation vocibus platonem nam, id cum eligendi deseruisse, mea clita vitae sanctus no. Legere debitis vel ne. Impetus vivendo voluptatibus vim ne. Audire tacimates sea id, labores volumus scribentur eos no. Ridens putent maluisset ei nam, has et purto recusabo, dicat labitur electram pro id. Offendit luptatum tincidunt ne mea, per no discere placerat nominati.
+
+                Pro no nonumy saperet. Volumus argumentum et vel, at nam possim habemus. Postea volumus concludaturque eam eu, cum labore iisque ex. Cu illud causae facilis mei, mea ei mucius timeam corpora. Has reque persequeris an, cum in tale choro urbanitas.
+
+                Te erant vulputate eam, debitis verterem qui no, sed denique torquatos ex. Vocent denique adipisci pri ei, et est appareat percipit mnesarchum. Pri quot theophrastus et. Cum eirmod partiendo consequuntur cu. Te atqui ullamcorper qui, nec atomorum ocurreret ex, eu utinam quidam eos.
+
+                Ne postulant efficiantur vel, everti regione sit cu, option albucius accusamus cum cu. Graeci contentiones has et, ut ius aliquip imperdiet consetetur, nullam ignota propriae in vel. Quo magna sententiae in, diam duis cu nec. Has no nostrud mnesarchum, ad aliquid dolorem nec.
+
+                At quod facer praesent mei. Id doctus molestie his, justo solet per id. Dicat volumus postulant ne his, soleat veritus referrentur te cum, mel no elitr tamquam numquam. Sint consectetuer vix at, cu vim nullam neglegentur. His ei quot errem laoreet, nam ei vidit dicunt. Ad repudiare dignissim mea, ei eum rebum rationibus adipiscing.
+            </p>
+        </article>
+    </div>
+</section>
