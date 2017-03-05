d6508fb90b860fe855c8d2aeaa8d584eef3ab005
william <wnfukunaga@gmail.com>
2017-03-05 23:55:41 +0000

    added new article page

---
+++ pages/add_article.php
@@ -0,0 +1,9 @@
+<?php
+    session_start();
+
+    $cssStyle = "../styles/addarticlestyle.css";
+
+    require('../templates/header.php');
+    require('../templates/add_article.html');
+    require('../templates/footer.php');
+?>
