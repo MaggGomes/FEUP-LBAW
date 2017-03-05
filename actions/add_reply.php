a430238f92c859838c5d1ab17493fba74812bbf3
MaggGomes <magg.slb@gmail.cm>
2017-03-05 05:28:42 +0000

    new clean version - homepage, article page, and login/register being worked

--- actions/add_reply.php
+++
@@ -1,9 +0,0 @@
-<?php
-    include_once('../database/connection.php');
-    include_once('../database/reply.php');
-
-    insertReply($db, $_POST['idReview'], $_POST['idUsername'], $_POST['inputreview']);
-
-    $location = "../pages/restaurant.php?id=" . $_POST['idRestaurant'];
-    header("Location: " . $location);
-?>
