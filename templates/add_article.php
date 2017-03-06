3133b406835ec124dbd56cde8ccc1262eb2e38bb
MaggGomes <magg.slb@gmail.cm>
2017-03-06 21:13:10 +0000

    small fix to add-article

--- templates/add_article.php
+++ templates/add_article.php
@@ -3,22 +3,85 @@
     <script>tinymce.init({ selector:'textarea' });</script>
 </head>
 <section class="central">
-    <div class="container">
+
+
+    <div class="container panel panel-default">
+        <article id="add-article">
+            <div class="container row">
+                <form action="../actions/action_add_article.php" method="post" class="row form-group">
+                    <div class="container row">
+                        <div class="col-md-4">
+                            <input type="text" name="title" class="form-control" placeholder="Title" required>
+                        </div>
+                    </div>
+                    <div class="container row">
+                        <div class="col-md-4">
+                            <input type="text" name="title" class="form-control" placeholder="Description" required>
+                        </div>
+                    </div>
+
+                    <div class="col-lg-12">
+                        <textarea rows="10" name="body" class="col-lg-12" required></textarea>
+                    </div>
+                    <div class="col-lg-12">
+                        <h4>Category</h4>
+
+                        <div class="btn-group" data-toggle="buttons">
+                            <label class="btn btn-primary active radio-inline">
+                                <input type="radio" name="options" id="option1" autocomplete="off" checked>World
+                            </label>
+                            <label class="btn btn-primary radio-inline">
+                                <input type="radio" name="options" id="option2" autocomplete="off">Sports
+                            </label>
+                            <label class="btn btn-primary radio-inline">
+                                <input type="radio" name="options" id="option3" autocomplete="off"> Technology
+                            </label>
+                            <label class="btn btn-primary radio-inline">
+                                <input type="radio" name="options" id="option4" autocomplete="off">Politics
+                            </label>
+                            <label class="btn btn-primary radio-inline">
+                                <input type="radio" name="options" id="option5" autocomplete="off">Culture
+                            </label>
+                            <label class="btn btn-primary radio-inline">
+                                <input type="radio" name="options" id="option6" autocomplete="off">Science
+                            </label>
+                        </div>
+                    </div>
+                    <div class="container row">
+                        <div class="col-md-4">
+                            <h4>Tags</h4>
+                            <input type="text" class="form-control col-lg-12" name="tags" placeholder="Write a tag">
+                        </div>
+                    </div>
+                    <div class="container row">
+                        <div class="col-md-2">
+                            <button type="submit" class="btn btn-primary btn-block" disabled>Publish</button>
+                        </div>
+                    </div>
+                </form>
+            </div>
+        </article>
+    </div>
+
+
+
+
+   <!-- <div class="container">
         <article id="addrestaurant">
             <div class="sectiondescription">
                 <h3>New article<h3>
             </div>
-            <article class="box row">
+            <article class="row">
                 <form action="../actions/action_add_restaurant.php" method="post" class="row">
                     <div class="col-lg-12">
                         <h4>Title</h4>
-                        <input type="text" name="title" class="col-lg-12" placeholder="Article title" required>
+                        <input type="text" name="title" class="form-control col-lg-12" placeholder="Article title" required>
                     </div>
                     <div class="col-lg-12">
                         <h4>Field</h4>
                         <textarea rows="10" name="body" class="col-lg-12" required></textarea>
                     </div>
-                    <div class="col-lg-12 ">
+                    <div class="col-lg-12">
                         <h4>Category</h4>
 						<div class="row">
 	                        <div class="col-md-2">
@@ -50,7 +113,7 @@
 					</div>
                     <div class="col-lg-12">
                         <h4>Tags</h4>
-                        <input type="text" class="col-lg-12" name="tags">
+                        <input type="text" class="form-control col-lg-12" name="tags">
                     </div>
                     <div class="col-lg-12">
                         <button type="submit" class="btn btn-primary btn-block" disabled>Create article</button>
@@ -58,5 +121,5 @@
                 </form>
             </article>
         </article>
-    </div>
+    </div>-->
 </section>
