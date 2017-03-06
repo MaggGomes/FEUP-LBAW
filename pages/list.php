bf12463f0e419de4f31c87281b93d68780b0712b
william <wnfukunaga@gmail.com>
2017-03-06 19:07:23 +0000

    added list form template

---
+++ pages/list.php
@@ -0,0 +1,9 @@
+<?php
+    session_start();
+
+    $cssStyle = "../styles/list.css";
+
+    require('../templates/header.php');
+    require('../templates/list.php');
+    require('../templates/footer.php');
+?>
---
+++ templates/list.php
@@ -0,0 +1,27 @@
+<section class="central">
+    <div class="container">
+        <article id="addrestaurant">
+            <div class="sectiondescription">
+                <h3>List of something<h3>
+            </div>
+            <div class="list-group">
+				<div class="list-group-item list-group-item-info">
+					<div class="row">
+						<a href="#" class="col-md-6">Name</a>
+						<a href="#" class="col-md-2">Rating</a>
+						<a href="#" class="col-md-2">Publisher</a>
+						<a href="#" class="col-md-2">Views</a>
+					</div>
+				</div>
+				<a href="#" class="list-group-item list-group-item-action">
+					<div class="row">
+						<label class="col-md-6">Big Short</label>
+						<div class="col-md-2">4.34</div>
+						<div class="col-md-2">Rui asdad</div>
+						<div class="col-md-2">45</div>
+					</div>
+				</a>
+            </div>
+        </article>
+    </div>
+</section>
