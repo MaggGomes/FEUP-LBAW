2233f942d44d5a1a44773d7c8e70392ae9ed7b67
williamnf <wnfukunaga@gmail.com>
2017-03-08 16:33:44 +0000

    added advanced search

---
+++ templates/advanced_search.php
@@ -0,0 +1,76 @@
+<section class="central">
+    <div class="container">
+        <article id="addrestaurant">
+            <div class="sectiondescription">
+                <h3>List of something<h3>
+            </div>
+            <div class="row">
+                <div class="col-md-3">
+                    <form class="" action="index.html" method="post">
+                        <!-- <div class="btn-group">
+                            <button type="button" class="btn btn-default dropdown-toggle" data-toggle="dropdown" name="button" aria-haspopup="true" aria-expanded="false">
+                                Search Users <span class="caret"></span>
+                            </button>
+                            <ul class="dropdown-menu">
+                                <li>Search Users</li>
+                                <li>Search Article Title</li>
+                                <li>Search Article content</li>
+                            </ul>
+                        </div> -->
+
+                        <select class="btn btn-primary form-control input-md" name="type">
+                            <option value="users">Search Users</option>
+                            <option value="title">Search Article Title</option>
+                            <option value="content">Search Article Content</option>
+                        </select>
+                        <br><br>
+
+
+                        <div class="row col-md-offset-1">
+                            <label for="">Category</label>
+                            <div class="">
+                                <input type="checkbox" class="" name="" value=""> Sporting
+                                <input type="checkbox" class="" name="" value=""> Espinho
+                            </div>
+                            <div class="">
+                                <input type="checkbox" name="" value=""> Sporting
+
+                                    <input type="checkbox" name="" value=""> Sporting
+                            </div><div class="">
+                                <input type="checkbox" name="" value=""> Sporting
+
+                                    <input type="checkbox" name="" value=""> Sporting
+                            </div>
+                        </div>
+                        <br>
+                        <div class="">
+                            <input type="textarea" name="tags" rows="5" placeholder="tags" class="form-control input-md" value="">
+                        </div>
+                    </form>
+                </div>
+                <div class="col-md-9">
+                    <input type="text" name="content" placeholder="Search in article" class="form-control input-md" ></input>
+                    <br>
+                    <div class="list-group">
+                        <div class="list-group-item list-group-item-info">
+                            <div class="row">
+                                <button href="#" class="btn btn-link col-md-6">Name</button>
+                                <button href="#" class="btn btn-link col-md-2">Rating</button>
+                                <button href="#" class="btn btn-link col-md-2">Publisher</button>
+                                <button href="#" class="btn btn-link col-md-2">Views</button>
+                            </div>
+                        </div>
+                        <a href="#" class="list-group-item list-group-item-action">
+                            <div class="row">
+                                <div class="col-md-6">Big Short</div>
+                                <div class="col-md-2">4.34</div>
+                                <div class="col-md-2">Rui asdad</div>
+                                <div class="col-md-2">45</div>
+                            </div>
+                        </a>
+                    </div>
+                </div>
+            </div>
+        </article>
+    </div>
+</section>
