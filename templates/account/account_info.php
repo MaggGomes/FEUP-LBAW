aa3f35ca505e5ae15056ed0a316e9242185c890d
williamnf <wnfukunaga@gmail.com>
2017-03-08 19:05:56 +0000

    added templates for admin page

---
+++ templates/account/account_info.php
@@ -0,0 +1,42 @@
+<section>
+    <div class="row">
+        <div class="col-md-offset-1">
+            <div class="col-md-12">
+                <div class="col-md-12">
+                    <div class="col-md-6">Change name</div>
+                    <div class="col-md-6"><input type="text" name="name" placeholder="New name..."></div>
+                </div>
+                <div class="col-md-12">
+                    <div class="col-md-6">Change password</div>
+                    <div class="col-md-6"><input type="password" name="name" placeholder="New name..."></div>
+                </div>
+                <div class="col-md-12">
+                    <div class="col-md-6">Confirm new password</div>
+                    <div class="col-md-6"><input type="password" name="name" placeholder="New name..."></div>
+                </div>
+                <div class="col-md-12">
+                    <div class="col-md-6">Other changes</div>
+                    <div class="col-md-6"><input type="text" name="name" placeholder="New name..."></div>
+                </div>
+                <div class="col-md-12">
+                    <div class="col-md-6">Change name</div>
+                    <div class="col-md-6"><input type="text" name="name" placeholder="New name..."></div>
+                </div>
+                <div class="col-md-12">
+                    <div class="col-md-6">Notifications</div>
+                    <div class="col-md-6">
+                        <select class="" name="">
+                            <option value="Always">Always</option>
+                            <option value="When referenced">When referenced</option>
+                            <option value="Another">Another</option>
+                        </select>
+                    </div>
+                </div>
+                <div class="col-md-12">
+                    <div class="col-md-12"><button type="submit" name="button" class="btn btn-primary btn-block" >Update profile</button></div>
+                </div>
+            </div>
+        </div>
+
+    </div>
+</section>
