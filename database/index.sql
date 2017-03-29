90bfbc5f131d3998cff824c8464be0dc8ea11837
JJPO96 <jose.j.p.oliveira.96@gmail.com>
2017-03-29 20:32:30 +0100

    indexes changed

--- database/index.sql
+++ database/index.sql
@@ -1,10 +1,7 @@
-DROP INDEX IF EXISTS users;
 DROP INDEX IF EXISTS report;
 DROP INDEX IF EXISTS suspensions;
 DROP INDEX IF EXISTS acceptedArticles;
-
-
-CREATE INDEX users USING hash ON Users(name ASC);
+DROP INDEX IF EXISTS articleImages;
 
 CREATE INDEX report USING hash ON Report(category);
 
@@ -12,3 +9,5 @@ CREATE INDEX suspensions USING hash ON Suspension(idUser);
 
 CREATE INDEX acceptedArticles USING hash ON Article(visibility)
     WHERE (visibility == "Visible");
+
+CREATE INDEX articleImages USING hash ON Image(idArticle);
