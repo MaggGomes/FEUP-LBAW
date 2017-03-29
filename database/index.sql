1895e82fe44703d6ae3c8daa2961eaacc14379f3
JJPO96 <jose.j.p.oliveira.96@gmail.com>
2017-03-29 20:44:34 +0100

    a

--- database/index.sql
+++ database/index.sql
@@ -2,6 +2,7 @@ DROP INDEX IF EXISTS report;
 DROP INDEX IF EXISTS suspensions;
 DROP INDEX IF EXISTS acceptedArticles;
 DROP INDEX IF EXISTS articleImages;
+DROP INDEX IF EXISTS articleDateCreation;
 
 CREATE INDEX report USING hash ON Report(id);
 
@@ -11,3 +12,5 @@ CREATE INDEX acceptedArticles USING hash ON Article(idArticle)
     WHERE (visibility == "Visible");
 
 CREATE INDEX articleImages USING hash ON Image(idArticle);
+
+CREATE INDEX articleDateCreation on Article USING btree(datacolocacao);
