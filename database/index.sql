aed6c85c9ad4c95aefc046bbd9acb2bf1d5dee05
JJPO96 <jose.j.p.oliveira.96@gmail.com>
2017-03-29 20:35:48 +0100

    as

--- database/index.sql
+++ database/index.sql
@@ -3,11 +3,11 @@ DROP INDEX IF EXISTS suspensions;
 DROP INDEX IF EXISTS acceptedArticles;
 DROP INDEX IF EXISTS articleImages;
 
-CREATE INDEX report USING hash ON Report(category);
+CREATE INDEX report USING hash ON Report(id);
 
 CREATE INDEX suspensions USING hash ON Suspension(idUser);
 
-CREATE INDEX acceptedArticles USING hash ON Article(visibility)
+CREATE INDEX acceptedArticles USING hash ON Article(idArticle)
     WHERE (visibility == "Visible");
 
 CREATE INDEX articleImages USING hash ON Image(idArticle);
