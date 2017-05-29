81b6daa37d0b0f6282174c1df85499b3082e0ced
JJPO96 <jose.j.p.oliveira.96@gmail.com>
2017-05-29 17:59:49 +0100

    report article changed to work with articles and comments

---
+++ final/actions/report.php
@@ -0,0 +1,22 @@
+<?php
+	include_once("../config/init.php");
+
+	$userID = $_SESSION["id"];
+	$reportID = $_POST["repID"];
+	$description = $_POST["description"];
+	$reportType = $_POST["type"]
+
+	console_log($userID);
+
+
+	if ($reportType === "article") {
+		$stmt = $conn->prepare("INSERT INTO report (description,state,idArticle,idUser) VALUES (?, "Pending", ?, ?)");
+		$stmt->execute(array($description, $reportID, $userID));
+	} elseif ($reportType === "comment") {
+			#$stmt = $conn->prepare("INSERT INTO report (description,state,idArticle,idUser) VALUES (?, "Pending", ?, ?)");
+			#$stmt->execute(array($description, $reportID, $userID));
+	}
+	$result = $stmt->fetch();
+
+	header("Location: ../pages/home.php");
+?>
